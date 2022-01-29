3bf7b21c7eee1780c58d6ff04d0d6b208780743f
Quinn Feinberg <qfeinberg@greatriverschool.org>
2022-01-29 14:39:18 -0600

    added the solenoids and motors to the constants

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -9,8 +9,13 @@ public final class Constants {
      * TODO: link
      */
     public final class Climber {
-
+       public static final int breakChannel = 0;
+       public static final int armForwardChannel = 0;
+       public static final int armReverseChannel = 0;
+       public static final int leftWinchID = 0;
+       public static final int rightWinchID = 0;
     }
+
 
     /**
      * drivetrain substyem constants
