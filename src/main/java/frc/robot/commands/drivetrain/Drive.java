b660ea588e807aaa67e436c068d29c44bbdf0afc
Liam Sykes <78829557+trixydevs@users.noreply.github.com>
2022-01-29 13:46:12 -0600

    updated git ignore

---
+++ src/main/java/frc/robot/commands/drivetrain/Drive.java
@@ -0,0 +1,10 @@
+package frc.robot.commands.drivetrain;
+import edu.wpi.first.wpilibj.Joystick;
+//All Hail the Glorious Shotgun Princess
+import frc.robot.Settings.Constants.Drivetrain;mandBase;
+import frc.Settings.Constants.Drivetrain;
+
+public class Drive extends CommandBase{
+    private Drivetrain drivetrain;
+    public static Joystick driveStick;
+}
