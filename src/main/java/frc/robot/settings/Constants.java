195b8b75f86341df94a881872584a66d5a298ab0
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-02-01 12:52:51 -0600

    Java final syntax and docs

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -1,28 +1,44 @@
 package frc.robot.settings;
 
 /**
- * Robot system constants
+ * <p>
+ * Robot system constants. Solenoids have a CHANNEL, motor controllers have an
+ * ID
+ * </p>
+ * 
+ * <p>
+ * Import this class using <code>import static Constants.Drivetrain.*</code> to
+ * use a constant without the
+ * <code>Constants.Drivetrain</code> qualifier. The patter works with wildcards
+ * or single static variables
+ * </p>
  */
 public final class Constants {
+    private Constants() {
+    }
+
     /**
      * climber substyem constants
      * TODO: link
      */
     public final class Climber {
-       public static final int breakChannel = 0;
-       public static final int armForwardChannel = 0;
-       public static final int armReverseChannel = 0;
-       public static final int leftWinchID = 0;
-       public static final int rightWinchID = 0;
-    }
+        private Climber() {
+        }
 
+        public static final int BREAK_CHANNEL = 0;
+        public static final int ARM_FORWARD_CHANNEL = 0;
+        public static final int ARM_REVERSE_CHANNEL = 0;
+        public static final int LEFT_WINCH_ID = 0;
+        public static final int RIGHT_WINCH_ID = 0;
+    }
 
     /**
      * drivetrain substyem constants
      * TODO: link
      */
     public final class Drivetrain {
-
+        private Drivetrain() {
+        }
     }
 
     /**
@@ -30,6 +46,8 @@ public final class Constants {
      * TODO: link
      */
     public final class Intake {
+        private Intake() {
+        }
 
     }
 }
