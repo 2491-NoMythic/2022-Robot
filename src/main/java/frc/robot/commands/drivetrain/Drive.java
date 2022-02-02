a975684c055ac3b6d16de3bf87151952bd469c18
Liam Sykes <78829557+trixydevs@users.noreply.github.com>
2022-02-01 18:04:52 -0600

    added drive command

--- src/main/java/frc/robot/commands/drivetrain/Drive.java
+++ src/main/java/frc/robot/commands/drivetrain/Drive.java
@@ -1,10 +1,40 @@
 package frc.robot.commands.drivetrain;
-import edu.wpi.first.wpilibj.Joystick;
-//All Hail the Glorious Shotgun Princess
-import frc.robot.Settings.Constants.Drivetrain;mandBase;
-import frc.Settings.Constants.Drivetrain;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Drive extends CommandBase{
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+//All Hail the Glorious Shotgun Princess
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Drivetrain;
+
+public class Drive implements Command {
     private Drivetrain drivetrain;
-    public static Joystick driveStick;
+    double turnSpeed, lastLeftSpeed, lastRightSpeed; 
+    
+    double currentLeftSpeed = 0;
+    double currentRightSpeed = 0;
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        Set<Subsystem> requirements = new HashSet<Subsystem>();
+        requirements.add(drivetrain);
+        return requirements;
+    }
+
+    public Drive(Drivetrain drivetrain)
+    {
+        this.drivetrain = drivetrain;
+    }
+
+    public void execute()
+    {
+        drivetrain.setDrive(currentLeftSpeed, currentRightSpeed);
+    }
+
+
+
 }
+
+
+
