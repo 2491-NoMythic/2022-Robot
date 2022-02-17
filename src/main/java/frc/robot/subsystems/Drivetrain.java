a80bdb8b4cd9aab2814db7795ac4b41e31b5eaec
Programming 02 <2491nomythic@gmail.com>
2022-02-17 17:38:23 -0600

    This should run all the motors now,

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.settings.Constants.Drivetrain.*;
 
@@ -14,22 +15,20 @@ public class Drivetrain extends SubsystemBase{
     private WPI_TalonFX rightLeadMotor;
     private WPI_TalonFX rightFollowMotor;
     private DifferentialDrive bbDriveSystem;
+    private MotorControllerGroup leftMotors;
+    private MotorControllerGroup rightMotors;
     public Drivetrain() {
-/**
- * These are IDs.
- */
+
 		leftLeadMotor = new WPI_TalonFX(LEFT_LEAD_ID);
 		leftFollowMotor = new WPI_TalonFX(LEFT_FOLLOW_ID);
 		rightLeadMotor = new WPI_TalonFX(RIGHT_LEAD_ID);
 		rightFollowMotor = new WPI_TalonFX(RIGHT_FOLLOW_ID);
+        leftMotors = new MotorControllerGroup(leftLeadMotor, leftFollowMotor);
+        rightMotors = new MotorControllerGroup(rightLeadMotor, rightFollowMotor);
         addChild("Left Lead", leftLeadMotor);
         addChild("Left Follow", leftFollowMotor);
         addChild("Right Lead", rightLeadMotor);
         addChild("Right Follow", rightFollowMotor);
-
-        //Setting Followers
-		leftFollowMotor.follow(leftLeadMotor);
-		rightFollowMotor.follow(rightLeadMotor);
 
         
 		//making right motors go right
@@ -37,8 +36,8 @@ public class Drivetrain extends SubsystemBase{
 		rightFollowMotor.setInverted(InvertType.None);
 		leftLeadMotor.setInverted(InvertType.InvertMotorOutput);
 		leftFollowMotor.setInverted(InvertType.InvertMotorOutput);
-        bbDriveSystem = new DifferentialDrive(leftLeadMotor, rightLeadMotor);
-        bbDriveSystem.setDeadband(0.04);
+        bbDriveSystem = new DifferentialDrive(leftMotors, rightMotors);
+         bbDriveSystem.setDeadband(0.04);
         addChild("Diff Drive", bbDriveSystem);
     }
     public void setDrive(ControlMode mode, double speed){
@@ -76,11 +75,11 @@ public class Drivetrain extends SubsystemBase{
         rightLeadMotor.set(mode, speed);
     }
     public void curvatureDrive(double xSpeed, double zRotation, boolean allowTurnInPlace){
-        bbDriveSystem.curvatureDrive(xSpeed, zRotation, allowTurnInPlace);
+         bbDriveSystem.curvatureDrive(xSpeed, zRotation, allowTurnInPlace);
     }
 
     @Override
     public void periodic() {
-        bbDriveSystem.feed();
+         bbDriveSystem.feed();
     }
 }
