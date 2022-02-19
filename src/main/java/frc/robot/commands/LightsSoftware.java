49e31abaa3df125c7a6b478e4c874f7a1d4b8212
Programming 02 <2491nomythic@gmail.com>
2022-02-19 14:35:24 -0600

    forgot to save

--- src/main/java/frc/robot/commands/LightsSoftware.java
+++ src/main/java/frc/robot/commands/LightsSoftware.java
@@ -1,16 +1,19 @@
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.LightsHardware;
+
 public class LightsSoftware extends CommandBase{
-private LightsHardware lights;
+   private LightsHardware lights;
    public LightsSoftware(LightsHardware lights){
-this.lights=lights;
+      this.lights=lights;
    }
-    @Override
-    public void execute() {
-        // Fill the buffer with the indicator
-        lights.leftballindicator();
-        lights.rightballindicator();
-        lights.prettylights();
-      }     
+
+   @Override
+   public void execute() {
+      // Fill the buffer with the indicator
+      lights.leftballindicator();
+      lights.rightballindicator();
+      lights.prettylights();
+   }     
 }
