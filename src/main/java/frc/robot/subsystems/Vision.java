a25a0e890d5986ceafb2242583196ab148ae0952
Maxim Taulbee Testing <maximtaulbee@gmail.com>
2022-02-26 14:48:33 -0600

    vision subsystem repair

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -11,29 +11,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Add your docs here. */
 public class Vision extends SubsystemBase {
-    NetworkTableEntry tv, tx, ty, ta, ts, tl, tshort, tlong, thor, tvert, getpipe;
+
+    NetworkTableEntry tv, tx, ty, ta, tshort, tlong, thor, tvert, getpipe;
+    NetworkTableInstance inst;
+    NetworkTable visionTable;
     public Vision() {
-      
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
-        NetworkTable visionTable = inst.getTable("limelight");
+        inst = NetworkTableInstance.getDefault();
+        visionTable = inst.getTable("limelight");
 
         // NetworkTableInstance.getdefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
-        tv = visionTable.getEntry("tv");
-        tx = visionTable.getEntry("tx");
-        ty = visionTable.getEntry("ty");
-        ta = visionTable.getEntry("ta");
-        ts = visionTable.getEntry("ts");
-        tl = visionTable.getEntry("tl");
-        tshort = visionTable.getEntry("tshort");
-        tlong = visionTable.getEntry("tlong");
-        thor = visionTable.getEntry("thor");
-        tvert = visionTable.getEntry("tvert");
-        getpipe = visionTable.getEntry("getpipe");
+        tv = visionTable.getEntry("tv"); // if there are any detections (1 or 0)
+        tx = visionTable.getEntry("tx"); // horizontal offset from crosshair to target (-27 to 27 degrees)
+        ty = visionTable.getEntry("ty"); // vertical offset from crosshair to target (-20.5 to 20.5 degrees)
+        ta = visionTable.getEntry("ta"); // target area (0% to 100% of image frame)
+        tshort = visionTable.getEntry("tshort"); // sidelength of shortest side of detection
+        tlong = visionTable.getEntry("tlong"); // sidelength of longest side of detection
+        thor = visionTable.getEntry("thor"); // horizontal sidelength of detection (0-320 pixels)
+        tvert = visionTable.getEntry("tvert"); // vertical sidelength of detection (0-320 pixels)
+        getpipe = visionTable.getEntry("getpipe"); // get current pipeline number
     }
 
 
     public double getHorizontalPos(){
-        
         double angle = tx.getDouble(0);
         return angle;
     }
