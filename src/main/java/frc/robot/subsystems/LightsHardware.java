683b610cb3cec5d46224465cbc4aaab5f7031758
Programming-1 <>
2022-03-12 14:06:48 -0600

    I re-installed the lights

--- src/main/java/frc/robot/subsystems/LightsHardware.java
+++ src/main/java/frc/robot/subsystems/LightsHardware.java
@@ -4,10 +4,8 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.FakePixie;
 public class LightsHardware extends SubsystemBase{
-    private final AddressableLED ballsensorLedsLeft;
-    private final AddressableLED ballsensorLedsRight;
-    private final AddressableLED decorLedsLeft;
-    private final AddressableLED decorLedsRight;
+    private final AddressableLED adressableLedsLeft;
+    private final AddressableLED adressableLedsRight;
     private AddressableLEDBuffer ballsensorBufferLeft;
     private AddressableLEDBuffer ballsensorBufferRight;
     private AddressableLEDBuffer decorBufferLeft;
@@ -15,24 +13,22 @@ public class LightsHardware extends SubsystemBase{
     private FakePixie pixie;
         public LightsHardware(){
       // Must be a PWM header, not MXP or DIO
-      ballsensorLedsLeft = new AddressableLED(6);
-      ballsensorLedsRight = new AddressableLED(7);
-      decorLedsLeft = new AddressableLED(8);
-      decorLedsRight = new AddressableLED(9);
+      adressableLedsLeft = new AddressableLED(6);
+      adressableLedsRight = new AddressableLED(7);
       // Reuse buffer
       // Length is expensive to set, so only set it once, then just update data
       ballsensorBufferLeft = new AddressableLEDBuffer(5);
-      ballsensorLedsLeft.setLength(ballsensorBufferLeft.getLength());
+      adressableLedsLeft.setLength(ballsensorBufferLeft.getLength());
       ballsensorBufferRight = new AddressableLEDBuffer(5);
-      ballsensorLedsRight.setLength(ballsensorBufferRight.getLength());
+      adressableLedsRight.setLength(ballsensorBufferRight.getLength());
       decorBufferLeft = new AddressableLEDBuffer(20);
-      decorLedsLeft.setLength(decorBufferLeft.getLength());
+      adressableLedsLeft.setLength(decorBufferLeft.getLength());
       decorBufferRight = new AddressableLEDBuffer(20);
-      decorLedsRight.setLength(decorBufferRight.getLength());
-      ballsensorLedsLeft.setData(ballsensorBufferLeft);
-      ballsensorLedsRight.setData(ballsensorBufferRight);
-      decorLedsLeft.setData(decorBufferLeft);
-      decorLedsRight.setData(decorBufferRight);
+      adressableLedsRight.setLength(decorBufferRight.getLength());
+      adressableLedsLeft.setData(ballsensorBufferLeft);
+      adressableLedsRight.setData(ballsensorBufferRight);
+      adressableLedsLeft.setData(decorBufferLeft);
+      adressableLedsRight.setData(decorBufferRight);
       //I would just like to say that I had 'We don't talk about Bruno' stuck in my head while I was coding this.
       pixie = new FakePixie();
 }
@@ -55,9 +51,9 @@ public void leftballindicator() {
         }
     }
 public void prettylights(){
-  for (var leftdecorlights = 0; leftdecorlights < decorBufferLeft.getLength(); leftdecorlights++){
+  for (var leftdecorlights = 6; leftdecorlights < decorBufferLeft.getLength(); leftdecorlights++){
       decorBufferLeft.setRGB(leftdecorlights, 50, 0, 50);
-  for (var rightdecorlights = 0; rightdecorlights < decorBufferRight.getLength(); rightdecorlights++){
+  for (var rightdecorlights = 6; rightdecorlights < decorBufferRight.getLength(); rightdecorlights++){
       decorBufferRight.setRGB(rightdecorlights, 50, 0, 50);
 }
 }
