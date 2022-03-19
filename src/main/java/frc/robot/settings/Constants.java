d181090223c69cfbe56c7b5de21fc68408ab106d
2491NoMythic <2491nomythic@gmail.com>
2022-03-19 14:28:50 -0500

    Added Cargo Filter command with pixy

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -49,7 +49,7 @@ public final class Constants {
         public static final int LIGHTS_BUTTON_ID = 6; // todo
         public static final int INTAKEUP_BUTTON_ID = 4;
         public static final int INTAKEDOWN_BUTTON_ID = 2;
-
+        public static final int INTAKEFILTER_BUTTON_ID = 1;
         public static final int RIGHT_IN_BUTTON_ID = 8;
         public static final int RIGHT_OUT_BUTTON_ID = 6;
         public static final int LEFT_IN_BUTTON_ID = 7;
