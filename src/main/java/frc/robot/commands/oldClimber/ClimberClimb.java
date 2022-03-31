1b878c5cdcaff43320ccda5fb6d65e86ae79f916
veggie2u <veggie2u@cyberward.net>
2022-03-30 21:39:44 -0500

    Builds now

--- src/main/java/frc/robot/commands/oldClimber/ClimberClimb.java
+++ src/main/java/frc/robot/commands/oldClimber/ClimberClimb.java
@@ -1,37 +1,30 @@
 package frc.robot.commands.oldClimber;
 
-//import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.ArmExtendState;
-//import frc.robot.settings.Constants.Ps4;
 import frc.robot.subsystems.OldClimber;
 import static frc.robot.settings.Constants.Climber.*;
 
 public class ClimberClimb extends CommandBase {
-  //private PS4Controller ps4controller;
   private OldClimber climber;
   private ArmExtendState state;
 
   /**
-   * 
    * Moves arm up and down
    * 
-   * @param ArmExtendState
-   *
+   * @param climber subsystem
+   * @param armState move climber arms UP or DOWN
    */
   public ClimberClimb(OldClimber climber, ArmExtendState armState) {
     this.climber = climber;
-   // this.ps4controller = ps4controller;
     addRequirements(climber);
     state = armState;
   }
-
 
   @Override
   public void execute() {
 
     switch (state) {
-
       case UP:
         climber.climberOut(CLARM_SPEED_OUT);
         break;
@@ -39,18 +32,15 @@ public class ClimberClimb extends CommandBase {
         climber.climberIn(CLARM_SPEED_IN);
         break;
     }
-
   }
-
 
   @Override
   public boolean isFinished() {
-      switch (state){
-
-      case OUT:
-        return climber.isClimberFullyOut();
-      case IN:
-        return climber.isClimberFullyIn();
+      switch (state) {
+        case UP:
+          return climber.isClimberFullyOut();
+        case DOWN:
+          return climber.isClimberFullyIn();
       }
       return false;
   }
