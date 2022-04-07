559c55ebdf1564b0839e1959a68fac245ce01c00
2491NoMythic <2491nomythic@gmail.com>
2022-04-07 17:23:36 -0500

    Revert "tilt and extend lowers intake"
    This reverts commit 4c1c27613f4ebb7e60885abe3bc7b9bee2838d16.

--- src/main/java/frc/robot/commands/oldClimber/Automate/TiltBackAndExtend.java
+++ src/main/java/frc/robot/commands/oldClimber/Automate/TiltBackAndExtend.java
@@ -5,29 +5,25 @@
 package frc.robot.commands.oldClimber.Automate;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.OldClimber;
 
 public class TiltBackAndExtend extends CommandBase {
   OldClimber climber;
-  Intake intake;
+  
   /** 
    * Tilts the arms back while simultaniously extending them. 
    * 
    * @return When the arms have extended fully.
    */
-  public TiltBackAndExtend(OldClimber climber, Intake intake) {
+  public TiltBackAndExtend(OldClimber climber) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.climber = climber;
-    this.intake = intake;
     addRequirements(climber);
-    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.setArmUp();
     climber.setArmDown();
     climber.setArmPostion(1.0);
   }
